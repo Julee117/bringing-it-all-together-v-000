0582563f7130f113222960b0a069950e1e22dd2c
Julianne Kim <juliannne@gmail.com>
2017-11-08 13:55:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,6 +66,11 @@ class Dog
 
     rows = DB[:conn].execute(sql, id)
     self.new_from_db(rows.first)
+
+    OR
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def self.find_by_name(name)
